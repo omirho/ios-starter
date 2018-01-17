5416b6f45cdc82d95e8c645838f724c68b7356ad
Pulkit Arora <pulkitarora26@gmail.com>
2018-01-17 19:11:33 +0530

    create xcode project

---
+++ ios-starter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-starter
+//
+//  Created by Pulkit Arora on 17/01/18.
+//  Copyright © 2018 Pulkit Arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
