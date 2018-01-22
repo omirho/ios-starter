499b7467c96d0e3f87c806856e9bf3624da769a0
Pulkit Arora <pulkitarora26@gmail.com>
2018-01-22 22:16:04 +0530

    update colors file

--- ios-starter/Generated Files/Constants/Colors.swift
+++ ios-starter/Generated Files/Constants/Colors.swift
@@ -1,9 +1,42 @@
-//
-//  Colors.swift
-//  ios-starter
-//
-//  Created by Pulkit Arora on 22/01/18.
-//  Copyright © 2018 Pulkit Arora. All rights reserved.
-//
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
 
-import Foundation
+#if os(OSX)
+  import AppKit.NSColor
+  typealias Color = NSColor
+#elseif os(iOS) || os(tvOS) || os(watchOS)
+  import UIKit.UIColor
+  typealias Color = UIColor
+#endif
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// swiftlint:disable operator_usage_whitespace
+extension Color {
+  convenience init(rgbaValue: UInt32) {
+    let red   = CGFloat((rgbaValue >> 24) & 0xff) / 255.0
+    let green = CGFloat((rgbaValue >> 16) & 0xff) / 255.0
+    let blue  = CGFloat((rgbaValue >>  8) & 0xff) / 255.0
+    let alpha = CGFloat((rgbaValue      ) & 0xff) / 255.0
+
+    self.init(red: red, green: green, blue: blue, alpha: alpha)
+  }
+}
+// swiftlint:enable operator_usage_whitespace
+
+// swiftlint:disable identifier_name line_length type_body_length
+struct ColorName {
+  let rgbaValue: UInt32
+  var color: Color { return Color(named: self) }
+
+  /// <span style="display:block;width:3em;height:2em;border:1px solid black;background:#eeeeee"></span>
+  /// Alpha: 100% <br/> (0xeeeeeeff)
+  static let primary = ColorName(rgbaValue: 0xeeeeeeff)
+}
+// swiftlint:enable identifier_name line_length type_body_length
+
+extension Color {
+  convenience init(named color: ColorName) {
+    self.init(rgbaValue: color.rgbaValue)
+  }
+}
