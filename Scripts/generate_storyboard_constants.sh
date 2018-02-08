03e6d57b066deb74cf0c532e6f7bd780c16f8ead
Pulkit Arora <pulkitarora26@gmail.com>
2018-02-08 16:02:29 +0530

    storyboard script changes to support multiple targets

--- Scripts/generate_storyboard_constants.sh
+++ Scripts/generate_storyboard_constants.sh
@@ -4,4 +4,4 @@ STORYBOARD_FOLDER_RELATIVE_PATH="./"
 TARGET_STORYBOARD_FOLDER_PATH="${SRCROOT}/${PROJECT_NAME}/${STORYBOARD_FOLDER_RELATIVE_PATH}"
 OUTPUT_STORYBOARD_PATH="${SRCROOT}/Generated/Constants/Storyboards.swift"
 
-${SRCROOT}/Tools/swiftgen/bin/swiftgen storyboards -t swift4 $TARGET_STORYBOARD_FOLDER_PATH --output $OUTPUT_STORYBOARD_PATH
+${SRCROOT}/Tools/swiftgen/bin/swiftgen storyboards -t swift4 $TARGET_STORYBOARD_FOLDER_PATH --param module=${PROJECT_NAME} --param ignoreTargetModule=true --output $OUTPUT_STORYBOARD_PATH
